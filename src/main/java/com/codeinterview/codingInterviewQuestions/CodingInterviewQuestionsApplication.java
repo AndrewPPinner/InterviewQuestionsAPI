d617699de2770c15925cf318b648c2be3e36fafa
Andrew Pinner <andppinner@gmail.com>
2022-03-14 16:05:26 -0400

    First commit

---
+++ src/main/java/com/codeinterview/codingInterviewQuestions/CodingInterviewQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.codeinterview.codingInterviewQuestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingInterviewQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingInterviewQuestionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeinterview/codingInterviewQuestions/CodingInterviewQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeinterview.codingInterviewQuestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingInterviewQuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
